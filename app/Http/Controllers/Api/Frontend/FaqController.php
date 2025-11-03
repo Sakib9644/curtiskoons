bcb989bb14c805d27b223004407847e68924bb56
Sakib9644 <17203075@iubat.edu>
2025-11-03 11:23:33 +0600

    faq_updated

--- app/Http/Controllers/Api/Frontend/FaqController.php
+++ app/Http/Controllers/Api/Frontend/FaqController.php
@@ -6,11 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Models\FAQ;
 
 class FaqController extends Controller{
-    public function index(){
-        $faq = FAQ::where('status','==', 'active')->get();
-        $data = [
-            'faq' => $faq
-        ];
-        return Helper::jsonResponse(true, 'faq list', 200, $data);
-    }
+   public function index() {
+    $faq = FAQ::where('status', 'active')->get();
+
+    $data = ['faq' => $faq];
+
+    return Helper::jsonResponse(true, 'FAQ list', 200, $data);
+}
 }

c7bc0b3dd7bc9d4961089e28c97d94584aa999ed
Sakib9644 <17203075@iubat.edu>
2025-11-03 14:27:45 +0600

    update

--- app/Http/Controllers/Api/Frontend/FaqController.php
+++ app/Http/Controllers/Api/Frontend/FaqController.php
@@ -1,17 +1,25 @@
 <?php
+
 namespace App\Http\Controllers\Api\Frontend;
 
 use App\Helpers\Helper;
 use App\Http\Controllers\Controller;
 use App\Models\FAQ;
 
-class FaqController extends Controller{
-   public function index() {
-    
-    $faq = FAQ::select('question','answer')->get();
+class FaqController extends Controller
+{
+    public function index()
+    {
+        $faq = FAQ::select('question', 'answer')->get()->map(function ($item) {
+            // Remove HTML tags from answer
+            $item->answer = strip_tags($item->answer);
 
-    $data = $faq;
+            // Remove numbers and dots from beginning of question (like "01." or "02. ")
+            $item->question = preg_replace('/^\d+\.\s*/', '', $item->question);
 
-    return Helper::jsonResponse(true, 'FAQ list', 200, $data);
-}
+            return $item;
+        });
+
+        return Helper::jsonResponse(true, 'FAQ list', 200, $faq);
+    }
 }
